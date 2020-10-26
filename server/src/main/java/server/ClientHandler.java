7ce1dafe771075604a55926e266d3f69879da8da
nastia <nastiusha_super@mail.ru>
2020-10-26 22:47:52 +0300

    Homework 2

---
+++ server/src/main/java/server/ClientHandler.java
@@ -0,0 +1,141 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class ClientHandler {
+    DataInputStream in;
+    DataOutputStream out;
+    Server server;
+    Socket socket;
+
+    private String nickname;
+    private String login;
+
+    private final int TIME = 120000;
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Client connected " + socket.getRemoteSocketAddress());
+
+            new Thread(() -> {
+                try {
+                    socket.setSoTimeout(TIME);
+
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.startsWith("/reg ")) {
+                            String[] token = str.split("\\s");
+                            if (token.length < 4) {
+                                continue;
+                            }
+                            boolean b = server.getAuthService()
+                                    .registration(token[1], token[2], token[3]);
+                            if (b) {
+                                sendMsg("/regok");
+                            } else {
+                                sendMsg("/regno");
+                            }
+                        }
+
+                        if (str.startsWith("/auth ")) {
+                            String[] token = str.split("\\s");
+                            if (token.length < 3) {
+                                continue;
+                            }
+                            String newNick = server.getAuthService()
+                                    .getNicknameByLoginAndPassword(token[1], token[2]);
+                            if (newNick != null) {
+                                login = token[1];
+                                if (!server.isLoginAuthenticated(login)) {
+                                    nickname = newNick;
+                                    sendMsg("/authok " + newNick);
+                                    server.subscribe(this);
+                                    break;
+                                } else {
+                                    sendMsg("С этим логином уже вошли в чат");
+                                }
+                            } else {
+                                sendMsg("Неверный логин / пароль");
+                            }
+                        }
+                    }
+
+                    socket.setSoTimeout(0);
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                sendMsg("/end");
+                                break;
+                            }
+                            if (str.startsWith("/w ")) {
+                                String[] token = str.split("\\s", 3);
+                                if (token.length < 3) {
+                                    continue;
+                                }
+                                server.privateMsg(this, token[1], token[2]);
+                            }
+                        } else {
+                            server.broadcastMsg(this, str);
+                        }
+                        if (str.startsWith("/changenick ")) {
+                            String[] token = str.split("\\s", 2);
+                            if (token.length < 2) {
+                                continue;
+                            }
+                            if (server.getAuthService().updateNick(this.nickname, token[1])) {
+                                sendMsg("/yournick " + token[1]);
+                                sendMsg("Ваш новый ник " + token[1]);
+                                this.nickname = token[1];
+                                server.broadcastClientList();
+                            }
+                        }
+                    }
+                } catch (SocketTimeoutException | EOFException e) {
+                    System.out.println("Сойдинение закрыто");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    server.unsubscribe(this);
+                    System.out.println("Client disconnected " + socket.getRemoteSocketAddress());
+                    try {
+                        socket.close();
+                        in.close();
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
